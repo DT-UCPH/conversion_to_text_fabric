d272c87364eb5d28825d357f80d866d123fbcf70
Martijn Naaijer <martijn.naayer@chello.nl>
2022-05-15 16:35:03 +0200

    adapted app.py

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ class TfApp(App):
     def fmt_layoutRich(app, n, **kwargs):
         api = app.api
         F = api.F
-        material = F.text.v(n) or ""
-        layout = f'<span class="gap">{material}</span>' if material
-        return f"{layout} "
+        material = F.text.v(n)
+        
+        return f"{material} "
